6c528c9123bb0cf3c17e5a2652467b2d0900df0b
Chris Beldam <chrisgbeldam@gmail.com>
2018-02-14 10:51:37 +0000

    Fixed Clear Button

--- starterapp/js/app.js
+++ starterapp/js/app.js
@@ -1,6 +1,6 @@
 // Function which obtains the wikipedia data for the form
 let displayWikipediaData = () => {
-    let $linksElement = $('#links');
+    let $linksElement = $('#wikiLinks');
     let searchString = $('#searchString').val();
     let wikipediaURL = "https://en.wikipedia.org/w/api.php?action=opensearch&search=" + searchString + "&format=json&callback=wikiCallback";
     $.ajax({
@@ -11,26 +11,25 @@ let displayWikipediaData = () => {
            let linkList = res[1];
             linkList.forEach(function(item){ // Function is run each time, es6
                 let url = 'https://en.wikipedia.org/wiki/' + item;
-                $linksElement.append('<li style="margin:2%;" class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent wikiLinks" id="links"><a href="' + url + '" target="_blank">' + item + '</a></li>');
+                $linksElement.append('<li style="margin:2%; color:white; background-color: #232C3F;" class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent wikiLinks" id="wikilinks"><a href="' + url + '" target="_blank" style="color:white;" id="wikiURL">' + item + '</a></li>');
                 return url;
             })
-           
+            $('#clearScreen').prop('disabled', false);
+            $('#clearScreen').css('background-color', '#232C3F');
         }
     });
     return false;
+    $('#clearScreen').prop('disabled', false);
 };
 
 $('#form').submit(displayWikipediaData);
 
-// let linksExists = document.getElementsByClassName('wikiLinks');
-// if($('.wikiLinks').length <= 1){
-//     $('#clearScreen').prop('disabled', true);
-//     $('#clearScreen').css('background-color', 'grey');
-// }
-// else {
-//     $('#clearScreen').prop('disabled', false);
-// }
+if($('#wikiURL').length <= 1){
+    $('#clearScreen').prop('disabled', true);
+    $('#clearScreen').css('background-color', 'grey');
+}
+
 
 $('#clearScreen').click(function() {
-    $('#links').remove();
+    $('#wikiLinks').remove();
 });
